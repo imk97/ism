ef0b7db3848a664ca3404fe590f03fe274fe1c5a
imk97 <Administrator@WIN-GCV7094H5KB.megasoft.com.my>
2020-12-03 16:11:31 +0800

    crud articles

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-12-02 03:18:39 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-12-02T03:18:39+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-12-02T03:18:44+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-12-02T03:18:51+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
